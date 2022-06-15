3c1cfd3a0b67ee61d9db48c100577254f87462e2
maulanadaffaay <maulanadaffa@student.uns.ac.id>
2022-06-15 16:09:34 +0700

    memperbaiki link dashboard

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-Route::get('/dashboard ', [PegawaiController::class, 'indeks'] )->name('Pegawai');
+// Route::get('/dashboard ', [PegawaiController::class, 'indeks'] )->name('Pegawai');
 Route::get('/ ', [PegawaiController::class, 'indeks'] )->name('Pegawai');
 
 require __DIR__.'/auth.php';
